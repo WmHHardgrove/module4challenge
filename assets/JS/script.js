ea5ce3118b86e14635876798f83838d362f2162f
BillH <wmhhardgroveiv@gmail.com>
2022-05-09 17:55:54 -0400

    Timer added

---
+++ assets/JS/script.js
@@ -0,0 +1,21 @@
+var timerEl = document.getElementById('countdown');
+var button = document.querySelector('button');
+
+
+
+button.onclick = function countdown() {
+  var timeLeft = 10; 
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+countdown();
